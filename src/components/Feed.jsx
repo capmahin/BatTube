a5ae14a66ca87557d91c5e65e871e63f382e8a74
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-26 02:44:55 +0600

    add boarderright

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,8 +3,10 @@ import {Box, Stack, Typography} from '@mui/material';
 
 const Feed = () => {
   return (
-    <Stack sx={{flexDirection: {sm:"column", md:"row"}}}>
+    <Stack sx={{flexDirection: {sx:"column", md:"row"}}}>
+      <Box sm={{height:{sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px: {sx:0, md:2}}}>
 
+      </Box>
     </Stack>
   )
 }
