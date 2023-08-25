dc9511078e2c8de5ce82cfcca208df0939e83681
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-26 01:16:23 +0600

    add stack

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import {Stack} from '@mui/material';
 import {Link} from  'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
+import {logo} from '../utils/constants';
+
+const Navbar = () => 
+ (
+    <Stack direction="row" alignItems="center" p={2} sx={{position:'sticky', background:'#000', top:0, justifyContent:'space-between'}}>
+       
+    </Stack>
   )
-}
+
 
 export default Navbar
