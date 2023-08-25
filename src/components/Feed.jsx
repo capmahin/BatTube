a1a9aeaecf39f2b1e048223ebe12768420b710f0
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-26 02:56:45 +0600

    add Typography

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,8 +4,12 @@ import {Box, Stack, Typography} from '@mui/material';
 const Feed = () => {
   return (
     <Stack sx={{flexDirection: {sx:"column", md:"row"}}}>
-      <Box sm={{height:{sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px: {sx:0, md:2}}}>
+      <Box sx={{height:{sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px: {sx:0, md:2}}}>
+          Sidebar
 
+          <Typography className="copyright" variant="body2" sx={{mt:1.5, color:'#fff'}}>
+          Copyright © 2023 Bat Tube
+          </Typography>
       </Box>
     </Stack>
   )
