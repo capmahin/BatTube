1b774e779162d0d68f9d164fb7b4df0bf64610e1
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-26 00:31:08 +0600

    its work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-// import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-const App = () => {
-  (
-    <BrowserRouter sx={{backgroundColor:'#000'}}>
-      <Box>
-       <Navbar/>
-       <Routes>
-        <Route path="/" exact element={<Feed/>}/>
-        <Route  path="/video/:id"  element={<VideoDetail/>}/>
-        <Route  path="/channel/:id"  element={<ChannelDetail/>}/>
-        <Route  path="/search/:searchTerm"  element={<SearchFeed/>}/>
-       </Routes>
-      </Box>
-    </BrowserRouter>
-  )
-}
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
-export default App
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
