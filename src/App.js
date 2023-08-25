61cef1ace405ee5aa5e872d52708e783a2084470
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-25 23:10:30 +0600

    add elements

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ const App = () => {
     <BrowserRouter sx={{backgroundColor:'#000'}}>
       <Box>
        Navbar
+       <Routes>
+        <Route path="/" exact element={<Feed/>}/>
+        <Route  path="/video/:id"  element={<VideoDetail/>}/>
+        <Route  path="/channel/:id"  element={<ChannelDetail/>}/>
+        <Route  path="/search/:searchTerm"  element={<SearchFeed/>}/>
+       </Routes>
       </Box>
     </BrowserRouter>
   )
