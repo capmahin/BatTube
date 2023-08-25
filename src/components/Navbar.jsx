08906e4b43ecde5c97f39bc18cae829f0f23f54b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-26 01:22:13 +0600

    add more items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import {logo} from '../utils/constants';
 const Navbar = () => 
  (
     <Stack direction="row" alignItems="center" p={2} sx={{position:'sticky', background:'#000', top:0, justifyContent:'space-between'}}>
-       
+       <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
+       </Link>
     </Stack>
   )
 
