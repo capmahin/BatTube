ec4044336ada81c1cab864e468cd155e8fee4fda
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-25 22:50:47 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+// import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from "@mui/material";
 
 const App = () => {
-  return (
-    <div>App</div>
+  (
+    <BrowserRouter sx={{backgroundColor:'#000'}}>
+      <Box>
+       Navbar
+      </Box>
+    </BrowserRouter>
   )
 }
 
