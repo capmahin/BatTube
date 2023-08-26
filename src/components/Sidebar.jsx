e6369971429fdc425eaf962ddfea89da087cea84
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-27 02:16:23 +0600

    add category list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,15 @@ const Sidebar = () =>
     sx={{
       overflowY:"auto",
       height:{sx:'auto', md:'95%'},
+      flexDirection:{md:'column'},
     }}
     >
-
+        {categories.map((category)=>(
+          <button>
+            <span>{category.icon}</span>
+            <span>{category.name}</span>
+          </button>
+        ))}
     </Stack>
   )
 
