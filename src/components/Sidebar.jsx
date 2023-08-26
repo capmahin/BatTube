ed2707c0f877579e328bff52ebf4f74cda6a4cad
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-27 02:11:03 +0600

    add new stack

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,18 @@ import {Stack} from '@mui/material';
 
 import {categories} from '../utils/constants';
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+const Sidebar = () => 
+  (
+    <Stack
+    direction= "row"
+    sx={{
+      overflowY:"auto",
+      height:{sx:'auto', md:'95%'},
+    }}
+    >
+
+    </Stack>
   )
-}
+
 
 export default Sidebar
