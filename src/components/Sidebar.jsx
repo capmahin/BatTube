5bb2f22b909760ff750601dc8e7f6f00ce7854ad
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-27 02:26:48 +0600

    add coloer

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import {Stack} from '@mui/material';
 
 import {categories} from '../utils/constants';
 
+const selectedCategory = 'New';
 const Sidebar = () => 
   (
     <Stack
@@ -13,7 +14,12 @@ const Sidebar = () =>
     }}
     >
         {categories.map((category)=>(
-          <button>
+          <button 
+          className="category-btn"
+          style={{
+            background: category.name === selectedCategory && '#FC1503'
+          }}
+          >
             <span>{category.icon}</span>
             <span>{category.name}</span>
           </button>
