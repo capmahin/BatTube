06e0004e1d491e99ced2353919fa87450cb723d8
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-28 01:08:55 +0600

    add option

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,13 +4,11 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
 const options = {
     url: BASE_URL,
     params: {
-      relatedToVideoId: '7ghhRHRP6t4',
-      part: 'id,snippet',
-      type: 'video',
+     
       maxResults: '50'
     },
     headers: {
-      'X-RapidAPI-Key': '2187ab2ef8msh8745446ceb89f1dp19767ejsnad8ab1e99dc6',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
