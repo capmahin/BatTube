0ecf2083542130917dc04deb21f69e56fad999f0
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-27 23:13:59 +0600

    add videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from '@mui/material';
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
 
 const Feed = () => {
   return (
@@ -12,10 +13,12 @@ const Feed = () => {
           </Typography>
       </Box>
 
-      <Box p={2} sx={{overflowY:'auto', height:'90vh'}}>
+      <Box p={2} sx={{overflowY:'auto', height:'90vh', flex:2}}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{color:'white'}}>
           New <span style={{color:'#F31503'}} >videos</span>
         </Typography>
+
+        <Videos/>
       </Box>
     </Stack>
   )
