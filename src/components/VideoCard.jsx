c2086c7ec7296712e588424209c9bd5218b6dd6e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-29 02:00:23 +0600

    working on vide

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+import {Typography, Card, CardContent, CardMedia} from '@mui/material';
 
-const VideoCard = () => {
+import { CheckCircle } from "@mui/icons-material";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from "../utils/constants";
+
+
+const VideoCard = ({video}) => {
+    console.log(video);
   return (
     <div>VideoCard</div>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,6 @@
 import {Stack,Box} from '@mui/material'
-import VideoCard from "./VideoCard";
-import ChannelCard from "./ChannelCard";
+import {VideoCard, ChannelCard} from "./";
+
 
 const Videos = ({videos}) => {
   console.log(videos);
@@ -9,7 +9,7 @@ const Videos = ({videos}) => {
           {videos.map((item, idx)=>(
             <Box key={idx}>
               {item.id.videoId && <VideoCard video={item}/>}
-              {item.id.channelId && <ChannelCard channelDetail={item}/>}
+              {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
             </Box>
           ))}
     </Stack>
