25f0682fc8354ac080ce83d41f4f040562d001ec
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-30 16:32:31 +0600

    add new veriant

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,12 +1,26 @@
 import {Typography,  CardContent, CardMedia,Box} from '@mui/material';
 
 import { CheckCircle } from "@mui/icons-material";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
+
+const ChannelCard = ({channelDetail}) => 
+  (
+    <Box
+    sx={{
+      boxShadow:'none',
+      borderRadius:'20px',
+
+    }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+       <CardContent sx={{display:'flex', flexDirection:'column', justifyContent:'center', textAlign: 'center', color:'#fff'}}>
+
+       </CardContent>
+      </Link>
+    </Box>
   )
-}
+
 
 export default ChannelCard
