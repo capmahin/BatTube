b1861cf523e128b48144771b813accc6ca5c455c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-08-30 16:16:49 +0600

    add and import

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import {Typography,  CardContent, CardMedia,Box} from '@mui/material';
+
+import { CheckCircle } from "@mui/icons-material";
+import {Link} from 'react-router-dom'
 
 const ChannelCard = () => {
   return (
