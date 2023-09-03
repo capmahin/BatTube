bd193f6f4af9ac13af7221d0d4b44ba962d8011a
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-03 23:38:52 +0600

    add more dependencu

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,11 @@
 import {useState, useEffect} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import ReactPlayer from 'react-player';
-import {Typography, Box, Stack} from '@mui/material'
+import {Typography, Box, Stack} from '@mui/material';
+import {CheckCircle} from '@mui/material';
+
+import {Video} from './';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   return (
