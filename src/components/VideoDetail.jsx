82e388697f66cb735571e2f9a8ab560d6515937e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-03 23:35:49 +0600

    add another depenmdenmc

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import {Typography, Box, Stack} from '@mui/material'
 
 const VideoDetail = () => {
   return (
