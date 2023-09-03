68b03a827dd19ef137b5da78f6bbd80369af27ee
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-09-04 00:03:47 +0600

    add url in realPlayer

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -13,14 +13,14 @@ const VideoDetail = () => {
 
   useEffect(()=>{
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-    .then((data)=>setVideoDetail(data.items[0]))
+    .then((data)=>setVideoDetail(data.items[0]));
   },[id])
   return (
     <Box minHeight="95vh">
         <Stack direction={{xs:'column', md:'row'}}>
            <Box flex={1}>
            <Box sx={{width: '100%', position:'sticky', top: '86px'}}>
-               <ReactPlayer/>
+               <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
            </Box>
            </Box>
         </Stack>
